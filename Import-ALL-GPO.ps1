19ac977d06de74c558656ba0307fbc0f7932e376
Graham Smith <gsmitheidw@gmail.com>
2021-09-08 10:30:50 +0100

    Update Import-ALL-GPO.ps1

--- Import-ALL-GPO.ps1
+++ Import-ALL-GPO.ps1
@@ -1,1 +1,17 @@
+<#  Requires the function Get-GPOBackup https://jdhitsolutions.com/blog/powershell/1460/get-gpo-backup/
+.  Use Get-GPOBackup function like this to get the policy names
+.  Get-GPOBackup -Path C:\path-to\policies | select Name | out-file C:\BackupNames.txt
+.  
+.#>
 
+# Path to backed up policies (unzipped)
+$bPath = 'C:\GroupPolicyBackup\'
+$names = Get-Content C:\BackupNames.txt
+
+# You may need a migration table if importing to a different domain/forest and there are are equivalent items to be mapped to from the old polcies:
+
+foreach ($name in $names) {
+
+Import-GPO -BackupGpoName $name -Path $bPath -MigrationTable C:\migration.migtable -TargetName $name -CreateIfNeeded
+
+}
