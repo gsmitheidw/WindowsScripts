9a730b8931fe766111f074a76a483b9aa02320ff
gsmitheidw <gsmitheidw@gmail.com>
2022-07-29 11:09:16 +0100

    tidy up

--- grab-choco-nupkgs.ps1
+++ grab-choco-nupkgs.ps1
@@ -5,31 +5,34 @@ $downloadpath = 'c:\choco'
 Set-Location $downloadpath
 
 function get-nupkg {
-param ([string]$parameter1)
-$source = "https://community.chocolatey.org/api/v2/package/$parameter1"
-$filename = [System.IO.Path]::GetFileName($source)
-$dest = "$downloadpath\$filename.nupkg"
+param ([string]$appname)
+    $source = "https://community.chocolatey.org/api/v2/package/$appname"
+    $filename = [System.IO.Path]::GetFileName($source)
+    $dest = "$downloadpath\$filename.nupkg"
 
-$wc = New-Object System.Net.WebClient
-$wc.DownloadFile($source, $dest)
+    $wc = New-Object System.Net.WebClient
+    $wc.DownloadFile($source, $dest)
 
-Add-Type -assembly "system.io.compression.filesystem"
-$zip = [io.compression.zipfile]::OpenRead("$dest")
-$file = $zip.Entries | where-object { $_.Name -eq "$filename.nuspec"}
-$stream = $file.Open()
+    # Give it a few seconds to download
+    Start-Sleep 5
 
-$reader = New-Object IO.StreamReader($stream)
-$text = $reader.ReadToEnd()
+    Add-Type -assembly "system.io.compression.filesystem"
+    $zip = [io.compression.zipfile]::OpenRead("$dest")
+    $file = $zip.Entries | where-object { $_.Name -eq "$filename.nuspec"}
+    $stream = $file.Open()
 
-$reader.Close()
-$stream.Close()
-$zip.Dispose()
+    $reader = New-Object IO.StreamReader($stream)
+    $text = $reader.ReadToEnd()
 
-#cast to xml
-[xml]$xml = $text
-$version = $xml.SelectNodes('/*/*') | Select-Object -ExpandProperty version
-# rename file to include version
-Move-Item -Path $downloadpath\$filename.nupkg -Destination $downloadpath\$filename.$version.nupkg
+    $reader.Close()
+    $stream.Close()
+    $zip.Dispose()
+
+    #cast to xml
+    [xml]$xml = $text
+    $version = $xml.SelectNodes('/*/*') | Select-Object -ExpandProperty version
+    # rename file to include version
+    Move-Item -Path $downloadpath\$filename.nupkg -Destination $downloadpath\$filename.$version.nupkg
 }
 
 $packagename = Read-Host -Prompt 'Packages to grab'                                                                   
